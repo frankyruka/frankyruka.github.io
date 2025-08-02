7050377d960a2716cbeea757cbdb73071686ec02
sasahershko <sarayhershko@gmail.com>
2025-08-02 13:25:32 +0200

    Demo reel casi acabada

--- frankyruka-portfolio/src/components/CursorComponent.jsx
+++ frankyruka-portfolio/src/components/CursorComponent.jsx
@@ -1,18 +1,32 @@
 'use client';
 import { motion } from 'framer-motion';
 import useMousePosition from '@/utils/mousePosition';
+import { Pause, Play } from 'lucide-react';
 
 export default function CursorFollower() {
     const { followerPosition } = useMousePosition();
 
+
     return (
-        <motion.div
-            className="fixed w-5 h-5 bg-black rounded-full pointer-events-none z-50"
-            style={{
-                top: followerPosition.y,
-                left: followerPosition.x,
-                transform: "translate(-50%, -50%)"
-            }}
-        />
+        <>
+            <motion.div
+                id='mouse'
+                className="fixed w-5 h-5 bg-black rounded-full pointer-events-none z-50 flex justify-center items-center"
+                style={{
+                    top: followerPosition.y,
+                    left: followerPosition.x,
+                    transform: "translate(-50%, -50%)"
+                }}
+            >
+                <span
+                    id='mouse-text'
+                    className='text-white flex justify-center items-center'
+                >
+                    <Pause id='pause-icon' className='w-2 h-2 opacity-0 absolute'/>
+                    <Play id='play-icon' className='w-2 h-2 opacity-0'/>
+                </span>
+            </motion.div >
+        </>
+
     );
 }
--- frankyruka-portfolio/src/components/animation/Animation.jsx
+++ frankyruka-portfolio/src/components/animation/Animation.jsx
@@ -1,42 +1,86 @@
 'use client';
 import FadeInStagger from "@/components/animations/FadeInStagger";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Animation() {
   const videoRef = useRef();
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const togglePlay = () =>{
+
+  const togglePlay = () => {
     if (!videoRef.current) return;
 
-    if(isPlaying){
+    if (isPlaying) {
       videoRef.current.pause();
-    }else{
+      // mostrar play
+      gsap.to("#pause-icon", { opacity: 0, scale: 0.5, duration: 0.2 });
+      gsap.to("#play-icon", { opacity: 1, scale: 1, duration: 0.2, delay: 0.1 });
+    } else {
       videoRef.current.play();
+      // mostrar pause
+      gsap.to("#play-icon", { opacity: 0, scale: 0.5, duration: 0.2 });
+      gsap.to("#pause-icon", { opacity: 1, scale: 1, duration: 0.2, delay: 0.1 });
     }
 
-    setIsPlaying(!isPlaying)
-  }
+    setIsPlaying(!isPlaying);
+  };
+
+  useEffect(() => {
+    const mouse = document.getElementById('mouse');
+  }, []);
+
+  useGSAP(() => {
+
+  }, [isPlaying]);
 
   return (
     <FadeInStagger>
       <div>
         <h1 className="font-magilio text-center text-black text-[100px]">
-          Animation
+          Demo Reel
         </h1>
-        <div className="relative flex justify-center items center w-full -mt-6">
-          <video
-            src="/media/animation/demo-reel.mp4"
-            autoPlay
-            loop
-            playsInline
-            ref={videoRef}
-            className="w-[80%] rounded-xl"
-          />
-          <div 
-            className="absolute inset-0 bg-white opacity-0 hover:opacity-40 transition duration-400"
+        <div
+          className="relative flex justify-center items center w-full -mt-6"
+        >
+          <div className="w-[80%] mb-10"
+            onMouseEnter={() => {
+              gsap.killTweensOf(["#pause-icon", "#play-icon"]); //para matar cualquier animacion que este corriendo o pendiente sobre un elemento
+              gsap.to(mouse, { scale: 8, duration: 0.3 });
+
+              if (isPlaying) {
+                gsap.to("#pause-icon", { opacity: 1, scale: 1, duration: 0.3 })
+              } else {
+                gsap.to("#play-icon", { opacity: 1, scale: 1, duration: 0.3 })
+              }
+            }
+            }
+            onMouseLeave={() => {
+              gsap.to(mouse, { scale: 1, duration: 0.3 });
+
+
+              gsap.to("#pause-icon", { opacity: 0, scale: 0, duration: 1 })
+
+              gsap.to("#play-icon", { opacity: 0, scale: 0, duration: 1 })
+
+            }}
             onClick={togglePlay}
-          />
+          >
+            <video
+              src="/media/animation/demo-reel.mp4"
+              autoPlay
+              loop
+              playsInline
+              ref={videoRef}
+              className="rounded-xl"
+            />
+            {/* <div
+              className="absolute inset-0 bg-white opacity-0 hover:opacity-0 transition duration-400"
+
+            /> */}
+          </div>
+
         </div>
 
         {/* <button> 
