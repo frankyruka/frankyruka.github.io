f601ae2d93ad8b470836613eeb2314a0474f7ff1
sasahershko <sarayhershko@gmail.com>
2025-08-12 00:09:22 +0200

    Update CursorComponent.jsx

--- frankyruka-portfolio/src/components/CursorComponent.jsx
+++ frankyruka-portfolio/src/components/CursorComponent.jsx
@@ -1,11 +1,22 @@
 'use client';
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 import useMousePosition from '@/utils/mousePosition';
 import { Pause, Play, Plus, Minus, ArrowLeft, ArrowRight } from 'lucide-react';
 
 export default function CursorFollower() {
     const { followerPosition } = useMousePosition();
+    const [isMobile, setIsMobile] = useState(false);
 
+    useEffect(() => {
+        const checkMobile = () => {
+            // detecta pantallas pequeñas o touch
+            return window.matchMedia('(max-width: 768px)').matches || 'ontouchstart' in window;
+        };
+        setIsMobile(checkMobile());
+    }, []);
+
+    if (isMobile) return null;
 
     return (
         <>
@@ -22,12 +33,12 @@ export default function CursorFollower() {
                     id='mouse-text'
                     className='text-white flex justify-center items-center'
                 >
-                    <Pause id='pause-icon' className='w-2 h-2 opacity-0 absolute'/>
-                    <Play id='play-icon' className='w-2 h-2 opacity-0'/>
-                    <Plus id='open-icon' className='w-1 h-1 opacity-0 absolute'/>
-                    <Minus id='close-icon' className='w-2 h-2 opacity-0 absolute'/>
-                    <ArrowRight id='arrow-right-icon' className='w-2 h-2 opacity-0 absolute'/>
-                    <ArrowLeft id='arrow-left-icon' className='w-2 h-2 opacity-0 absolute'/>
+                    <Pause id='pause-icon' className='w-2 h-2 opacity-0 absolute' />
+                    <Play id='play-icon' className='w-2 h-2 opacity-0' />
+                    <Plus id='open-icon' className='w-1 h-1 opacity-0 absolute' />
+                    <Minus id='close-icon' className='w-2 h-2 opacity-0 absolute' />
+                    <ArrowRight id='arrow-right-icon' className='w-2 h-2 opacity-0 absolute' />
+                    <ArrowLeft id='arrow-left-icon' className='w-2 h-2 opacity-0 absolute' />
                 </span>
             </motion.div >
         </>
