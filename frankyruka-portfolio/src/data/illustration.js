c10e607b1c6019026b9434663b6e41ae3a5aee8b
sasahershko <sarayhershko@gmail.com>
2025-09-27 12:53:41 +0200

    Update illustration.js

--- frankyruka-portfolio/src/data/illustration.js
+++ frankyruka-portfolio/src/data/illustration.js
@@ -10,6 +10,18 @@ export const ILLUSTRATION_BLOCKS = {
     },
     {
       type: "single",
+      images: ["/media/visdev/dessert-project/concept-02.jpg"],
+    },
+    {
+      type: "single",
+      images: ["/media/visdev/dessert-project/concept-01.jpg"],
+    },
+    {
+      type: "single",
+      images: ["/media/visdev/underwater-city/illustration-city.jpg"],
+    },
+    {
+      type: "single",
       images: ["/media/illustration/columpio.jpg"],
     },
     {
